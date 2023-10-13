4947794550b5c0d40dd489e8b5e78a2334a69041
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-10-13 23:12:28 +0200

    added comments to add context of usage

--- my-plugin/src/lib.rs
+++ my-plugin/src/lib.rs
@@ -26,7 +26,7 @@ impl PluginSayHello {
 // impl SayHelloService for PluginSayHello {
 //     // this errors with "future cannot be sent between threads safely"
 //     async fn say_hello(&self) {
-//         // this should enable you to call tokio::sleep
+//         // this should enable you to call tokio::sleep but EnterGuard is not Send :(
 //         // https://docs.rs/tokio/latest/tokio/runtime/struct.Handle.html#method.enter
 //         let _guard = self.handle.enter();
 //         println!("[{}] Hello from plugin!", self.id);
@@ -41,14 +41,16 @@ impl PluginSayHello {
 #[async_trait]
 impl SayHelloService for PluginSayHello {
     async fn say_hello(&self) {
-        println!("[{}] Hello from plugin!", self.id);
+        let id = self.id.clone();
         let _ = self
             .handle
-            .spawn(async move {
-                // this errors with "there is no reactor running, must be called from the context of a Tokio 1.x runtime"
-                // let _ = tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            .spawn_blocking(move || {
+                println!("[{}] Hello from plugin!", id);
+                // calling tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                // errors with "there is no reactor running, must be called from the context of a Tokio 1.x runtime"
                 let _ = sleep(Duration::new(1, 0));
-                println!("sleep 1");
+                println!("slept 1");
+                println!("[{}] Hello again from plugin!", id);
             })
             .await;
     }
